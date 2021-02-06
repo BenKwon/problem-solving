d785f58761b4ff4eeb85c518d306dbf84153d388
benkwon <kwon5891@gmail.com>
2021-02-06 19:04:26 +0900

    rgb거리 for문버전 수정

--- DP백준/RGB거리_다른풀이.java
+++ DP백준/RGB거리_다른풀이.java
@@ -2,14 +2,14 @@ package DP백준;
 
 import java.util.Scanner;
 
-//1149 for 문제 (백준 틀렸다고 나옴)
-public class rgb2 {
+//1149 for 문제
+public class RGH거리_다른풀이 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
         int[][] house = new int[num][3];
         int[][] dp_store = new int[num][3];
-        int min = 1001;
+        int min = 1000000;
         for (int i = 0; i < num; i++) {
             for (int k = 0; k < 3; k++) {
                 house[i][k] = scanner.nextInt();
@@ -27,7 +27,7 @@ public class rgb2 {
                     }
                 }
                 dp_store[i][j] = min + house[i][j];
-                min = 1001;
+                min = 1000000;
             }
         }
 
