b3451998a2c4213b7b398e252ce384aaf3fa5eb5
BenKwon <kwon5891@gmail.com>
2021-08-04 16:35:38 +0900

    2502 떡먹는 호랑이 설명 추가

--- 백준/DP/Main_떡먹는호랑이_2502.java
+++ 백준/DP/Main_떡먹는호랑이_2502.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * d날에 호랑이에게 준 떡의 개수가 k개라면
+ * d-1날에 호랑이에게 준 떡의 개수는 반드시  k/2+1개가 된다.
+ * 따라서 d-1날에 올 수 있는 떡의 갯수는 k/2 + 1개부터 k -1 개의 사이 이다
+ * d-1날에 떡의 갯수가 정해지면 나머지 날의 떡의 갯수는 정해지므로
+ * 반복문을 통하여 구한다.
+ * 다만 만약 a < b < c 라고 할때 a날에오는 떡의 갯수가 b날에 오는 떡의 갯수보다 많아지면 그 경우는 안되는 경우이므로
+ * 패스한다.
+ */
 public class Main_떡먹는호랑이_2502 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +30,7 @@ public class Main_떡먹는호랑이_2502 {
             rice_cake[d - 1] = i;
             for (int j = d - 2; j >= 1; j--) {
                 rice_cake[j] = rice_cake[j + 2] - rice_cake[j + 1];
-                if(rice_cake[j] > rice_cake[j+1] || rice_cake[j] < 0){
+                if(rice_cake[j] > rice_cake[j+1] || rice_cake[j] < 0){ //안되는 경우
                     solution_find = false;
                 }
             }
