4dfd4307025755ba08d2521d07c92166941637f7
BenKwon <kwon5891@gmail.com>
2021-09-27 20:46:34 +0900

    [BOJ] 이분그래프 풀이 주석 추가

--- 백준/그래프이론/Main_1707_이분그래프.java
+++ 백준/그래프이론/Main_1707_이분그래프.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
+/**
+ * 풀이 : DFS OR BFS, HashSet
+ * dfs를 이용하여 연결되어있는 노드들을 그룹화 한다. -> 각 그룹들을 ArrayList형태로 group 에 저장해놓음
+ * group에서 각 그룹을 하나씩 가져와서 dfs탐색을 하면서 각 노드들을 탐색하면서 왼쪽 Hashset에 들어가는지 오른쪽 Hashset에 들어가는지 체크한다.(각 노드들의 인접노드을 탐색하면서 set에 있는지 검사하면서)
+ * 만약 두 set다 들어갈수없다면 해당 testcase의 정답은 NO
+ */
 public class Main_1707_이분그래프 {
     //    static int[][] graph;
     static ArrayList<Integer>[] graph;
