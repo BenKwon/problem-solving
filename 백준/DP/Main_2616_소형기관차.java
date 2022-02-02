199cc977eea5c113cb4a78d036cb73a7c2714789
benkwon <kwon5891@gmail.com>
2022-02-02 17:03:06 +0900

    [BOJ] 2616 소형기관차

--- 백준/DP/Main_2616_소형기관차.java
+++ 백준/DP/Main_2616_소형기관차.java
@@ -10,42 +10,35 @@ public class Main_2616_소형기관차 {
     static int n,k;
     static int[] trains,sum;
     static int[][] dp;
-    static int[][] visit;
-    public static int dfs(int cur,int level){
-        if(visit[level][cur] == 1) return dp[level][cur];
-        if(level == 3) return sum[cur + k - 1] - sum[cur - 1];
-        int result = 0;
-        int start = cur + k - 1;
-        for (int i = start; i + k <= n; i++) {
-            result = Math.max(result, dfs(i, level + 1));
-        }
+//    static int[][] visit;
 
-        dp[level][cur] = result + (sum[cur + k - 1] - sum[cur - 1]);
-        visit[level][cur] = 1;
-        return dp[level][cur];
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         trains = new int[n + 1];
         sum = new int[n + 1];
-        visit = new int[4][n + 1];
-        dp = new int[4][n+1];
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            trains[i] = Integer.parseInt(st.nextToken());
-            sum[i] = trains[i] + sum[i- 1];
+        for (int i = 0; i < n; i++) {
+            trains[i + 1] = Integer.parseInt(st.nextToken());
+            sum[i + 1] = sum[i] + trains[i + 1];
         }
         k = Integer.parseInt(br.readLine());
-
-        for(int i = 1 ; i + k <= n; i++){
-            dfs(i, 1);
+        dp = new int[n + 1][4];
+        dp[k][1] = sum[k];
+        for (int i = k + 1; i <= n; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if(i < j * k) break;
+                else if(j == 1){
+                    int newValue = sum[i] - sum[i - k];
+                    dp[i][j] = Math.max(dp[i - 1][j], newValue);
+                }else { //else if (j == 2 || j == 3)
+                    int newValue = (sum[i] - sum[i - k]) + dp[i - k][j - 1];
+                    dp[i][j] = Math.max(dp[i - 1][j], newValue);
+                }
+//                System.out.printf("dp[%d][%d] : %d\n",i,j,dp[i][j]);
+            }
         }
-        int sol = Integer.MIN_VALUE;
-        for(int i = 1 ; i + k <= n; i++){
-            sol = Math.max(sol, dp[1][i]);
-        }
-        System.out.println(sol);
+        System.out.println(dp[n][3]);
 
     }
 }
