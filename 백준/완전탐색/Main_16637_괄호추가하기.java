54723d484e4c6af35ad758d2685ff9d88c730382
BenKwon <kwon5891@gmail.com>
2022-03-21 21:54:17 +0900

    [BOJ] 16638 괄호추가하기2

--- 백준/완전탐색/Main_16637_괄호추가하기.java
+++ 백준/완전탐색/Main_16637_괄호추가하기.java
@@ -21,6 +21,8 @@ public class Main_16637_괄호추가하기 {
 
         dfs(0);
         System.out.println(answer);
+//        calculateMultiply("-433*300+7+8*2");
+
     }
 
     static void dfs(int curIndex) {
@@ -28,6 +30,7 @@ public class Main_16637_괄호추가하기 {
             answer = Math.max(answer, calculate());
             return;
         }
+        System.out.println("curIndex = " + curIndex);
         // 1. 현재 index에서 괄호 시작하기
         bracket[curIndex][0] = 1;
         bracket[curIndex + 1][1] = 1;
@@ -38,10 +41,15 @@ public class Main_16637_괄호추가하기 {
         dfs(curIndex + 1);
     }
 
+
     static int calculate() {
         String newExpression = "";
         Stack<Character> st = new Stack<>();
+        System.out.println("----------------------------");
+        System.out.println("k = " + k);
         for (int i = 0; i < k; i++) {
+            System.out.println("i = " + i);
+            System.out.println(expression.charAt(i * 2));
             if (bracket[i][0] == 1) {
                 st.push('(');
                 st.push(expression.charAt(i * 2));
@@ -67,9 +75,11 @@ public class Main_16637_괄호추가하기 {
             stReverse.push(pop);
         }
 
+        System.out.println("newEx = " + newEx);
         boolean startBracket = false;
         while (!stReverse.isEmpty()) {
             char pop = stReverse.pop();
+            System.out.println("pop = " + pop);
             if (pop == '(') {
                 if (tmp.length() > 0) {
                     finalExpression += tmp;
@@ -86,11 +96,14 @@ public class Main_16637_괄호추가하기 {
             tmp += pop;
         }
         if (tmp.length() > 0) finalExpression += tmp;
-        return calculateExpression(finalExpression);
+
+        System.out.println("finalExpression = " + finalExpression);
+        return calculateExpression(calculateMultiply(finalExpression));
 //        return 0;
     }
 
     static int calculateExpression(String str) {
+        System.out.println("str = " + str);
         int idx = 0;
         int init = 1;
         String tmp = "";
@@ -103,8 +116,10 @@ public class Main_16637_괄호추가하기 {
             if (idx >= str.length()) break;
         }
         init *= Integer.parseInt(tmp);
+        System.out.println("init = " + init);
         for (int i = idx; i < str.length(); ) {
             char next = str.charAt(i);
+            System.out.println("next = " + next);
             int newValue = 1;
             if (str.charAt(i + 1) == '-') {
                 newValue = -1;
@@ -127,7 +142,67 @@ public class Main_16637_괄호추가하기 {
                 init += newValue;
             }
         }
+        System.out.println("initresult = " + init);
         return init;
+    }
+
+    static String calculateMultiply(String str){
+        System.out.println("start with : " + str);
+        for (int i = 0; i < str.length(); i++) {
+            char next = str.charAt(i);
+            if(next =='*'){
+                //left
+                String left = "";
+                int leftIndex = i - 1;
+                while(leftIndex >= 0){
+                    char nextLeft = str.charAt(leftIndex);
+                    if(Character.isDigit(nextLeft)){
+                        left += nextLeft;
+                    }else{
+                        if(nextLeft == '-' && leftIndex == 0){
+                            left += nextLeft;
+                            leftIndex--;
+                        } else if (nextLeft == '-' && (!Character.isDigit(str.charAt(leftIndex - 1)))) {
+                            left += nextLeft;
+                            leftIndex--;
+                        }
+                        break;
+                    }
+                    leftIndex--;
+                }
+                String tmpLeft = "";
+                for (int k = left.length()- 1; k >= 0; k--) {
+                    tmpLeft += left.charAt(k);
+                }
+                left = tmpLeft;
+                System.out.println("left = " + left);
+                System.out.println("leftIndex = " + leftIndex);
+                //right
+                String right = "";
+                int rightIndex = i + 1;
+                if(str.charAt(rightIndex) == '-'){
+                    right += '-';
+                    rightIndex++;
+                }
+                while(rightIndex < str.length()){
+                    char nextRight = str.charAt(rightIndex);
+                    if (Character.isDigit(nextRight)) {
+                        right += nextRight;
+                    }else{
+                        break;
+                    }
+                    rightIndex++;
+                }
+                System.out.println("right = " + right);
+                System.out.println("rightIndex = " + rightIndex);
+                String mulResult = (Integer.parseInt(right) * Integer.parseInt(left)) +"";
+                String newStr = str.substring(0, leftIndex + 1) + mulResult +str.substring(rightIndex, str.length());
+                System.out.println("newStr = " + newStr);
+                str = newStr;
+                i = 0;
+            }
+        }
+        return str;
     }
 
 }
